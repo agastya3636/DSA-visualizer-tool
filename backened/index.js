ae255269b3fd9291d79e3705fdc3c0283c15c619
Agastya Kumar Yadav <124435030+agastya3636@users.noreply.github.com>
2024-06-16 23:20:22 +0530

    selection sort

--- backened/index.js
+++ backened/index.js
@@ -8,6 +8,30 @@ app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
+app.post("/selection_sort", (req, res) => {
+    const arrayString = req.body.array;
+    if (!arrayString) {
+        return res.status(400).send({ error: "Array string is required" });
+    }
+    let array = arrayString.split(" ").map(Number);
+    let steps = [];
+    steps.push([...array]);
+    let n = array.length;
+    for (let i = 0; i < n-1; i++) {
+        let minIndex = i;
+        for (let j = i+1; j < n; j++) {
+            if (array[j] < array[minIndex]) {
+                minIndex = j;
+            }
+        }
+        let temp = array[i];
+        array[i] = array[minIndex];
+        array[minIndex] = temp;
+        steps.push([...array]);
+    }
+    res.send({ steps });
+})
+
 app.post("/bouble_sort", (req, res) => {
     const arrayString = req.body.array;
     console.log(req.body)
